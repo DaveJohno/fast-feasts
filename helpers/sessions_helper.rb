01374dbc0045711762f4753541443e8a92886eb9
DaveJohno <davejohno@hotmail.com>
2022-02-22 18:14:28 +1100

    log out working, shows the logged in user name

---
+++ helpers/sessions_helper.rb
@@ -0,0 +1,12 @@
+# Ruby conveention: methods with a question mark always return a boolean value
+def logged_in?
+  !!session['user_id']
+end
+
+def current_user
+  if logged_in?
+    find_user_by_id(session['user_id'])
+  else
+    nil
+  end  
+end
